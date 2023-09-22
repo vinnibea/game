baddd31c6c2cf7e2a10fc628cfdcd975391360e2
Roman Marunkevich <98581641+vinnibea@users.noreply.github.com>
2023-09-22 15:30:33 +0300

    Delete space-news directory

--- space-news/src/components/Context.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { ContextInterface } from "../types/types";
-import { useAPI } from "../hooks/useApi";
-
-export const MyContext = createContext<ContextInterface>({
-    articles: [],
-    useAPI,
-  });
